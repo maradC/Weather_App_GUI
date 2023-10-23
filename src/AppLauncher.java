eb6bfa7a5e1ff3934da0aa296ee809f38f00f707
maradC <117858924+maradC@users.noreply.github.com>
2023-10-23 09:01:37 -0400

    Update AppLauncher.java

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class AppLauncher {
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                new WeatherAppGUI().setVisible(true);
+//               System.out.println(WeatherApp.getLocationData("Honduras"));
+//                System.out.println(WeatherApp.getCurrentTime());
+            }
+
+        });
+    }
 }
